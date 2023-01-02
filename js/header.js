d9a71865e10914c91f5245bbc83e625c66378ad1
caglayan25 <caglayancoskuner4545@gmail.com>
2023-01-02 10:29:37 +0300

    card styled

--- js/header.js
+++ js/header.js
@@ -1,23 +1,10 @@
 const menuBtn=document.getElementById("hamburgerMenuBtn")
-const navBar=document.getElementById("navbar")
+const navBar=document.getElementById("navBar")
 
 menuBtn.addEventListener("click",()=>{
-    console.log(getComputedStyle(navBar).display)
-    if(getComputedStyle(navBar).display==="none"){
+    if(getComputedStyle(navBar,null).display === "none"){
         navBar.style.display = "block"
     }else{
         navBar.style.display = "none"
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-//getComputedStyle(navBar, null).display
